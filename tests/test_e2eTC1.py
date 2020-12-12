7b1724e2c7622f37a9a24c9d5a7e4ac176629b14
Ankur Puri <ankur.puri@cognizant.com>
2020-12-12 16:19:44 +0100

    added a comment

--- tests/test_e2eTC1.py
+++ tests/test_e2eTC1.py
@@ -32,6 +32,7 @@ class TestE2E(BaseClass):
         successmsg = self.driver.find_element_by_class_name("alert-success").text
         print(successmsg)
         assert "Success! Thank you!" in successmsg
+        #Trying to push changes to local git
         self.driver.get_screenshot_as_file("success.png")
 
 
