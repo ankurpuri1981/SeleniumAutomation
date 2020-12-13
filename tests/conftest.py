f10af216a441692e8be4219577153aad7405c458
ankurkpuri <ankurkpuri@gmail.com>
2020-12-13 12:44:56 +0000

    removed pyautogui import as not needed for the framework in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,6 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.firefox.webdriver import WebDriver
-from mss import mss
-import os
-os.environ['DISPLAY'] = ':0'
-import pyautogui
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
