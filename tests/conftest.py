9b6698b5c880180d2969b33d394037155f88d7d0
Ankur Puri <ankur.puri@cognizant.com>
2020-12-14 15:25:46 +0100

    local

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import selenium
 import pytest
 import time
+
+
 from selenium import webdriver
 import os
 from selenium.webdriver import ActionChains
@@ -12,31 +14,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import platform
+import pytest_html
 driver = None
-
 
 # Include screenshot for failed TC's in the report
 @pytest.mark.hookwrapper
-def pytest_runtest_makereport(item):
-    """
-        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-        :param item:
-        """
-    pytest_html = item.config.pluginmanager.getplugin('html')
-    outcome = yield
-    report = outcome.get_result()
-    extra = getattr(report, 'extra', [])
-
-    if report.when == 'call' or report.when == "setup":
-        xfail = hasattr(report, 'wasxfail')
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            file_name = report.nodeid.replace("::", "_") + ".png"
-            _capture_screenshot(file_name)
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                extra.append(pytest_html.extras.html(html))
-        report.extra = extra
+# def pytest_runtest_makereport(item):
+#     """
+#         Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+#         :param item:
+#         """
+#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, 'extra', [])
+#
+#     if report.when == 'call' or report.when == "setup":
+#         xfail = hasattr(report, 'wasxfail')
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             file_name = report.nodeid.replace("::", "_") + ".png"
+#             _capture_screenshot(file_name)
+#             if file_name:
+#                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+#                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
+#                 extra.append(pytest_html.extras.html(html))
+#         report.extra = extra
 
 def _capture_screenshot(name):
     chrome_options = webdriver.ChromeOptions()
--- tests/unittest.py
+++ tests/unittest.py
@@ -1,3 +1,7 @@
 import os
+import sys
 os.chdir("..")
-os.getcwd() + "\\TestData\\testData.xlsx"
+#os.getcwd() + "\\TestData\\testData.xlsx"
+from shutil import copyfile
+#print(os.path.dirname(sys.executable))
+copyfile('chromedriver.exe', str(os.path.dirname(sys.executable))+ "\\chromedriver.exe")
